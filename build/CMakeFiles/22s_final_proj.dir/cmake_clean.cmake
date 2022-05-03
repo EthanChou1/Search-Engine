f081861f59b25ad0a68403f8f131695d78f18702
EthanChou1 <89790344+EthanChou1@users.noreply.github.com>
2022-05-03 12:36:47 -0500

    Add files via upload

---
+++ build/CMakeFiles/22s_final_proj.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "22s_final_proj"
+  "22s_final_proj.pdb"
+  "CMakeFiles/22s_final_proj.dir/DocumentInfo.cpp.o"
+  "CMakeFiles/22s_final_proj.dir/catch_setup.cpp.o"
+  "CMakeFiles/22s_final_proj.dir/invertIndex.cpp.o"
+  "CMakeFiles/22s_final_proj.dir/main.cpp.o"
+  "CMakeFiles/22s_final_proj.dir/parseDocument.cpp.o"
+  "CMakeFiles/22s_final_proj.dir/porter2_stemmer.cpp.o"
+  "CMakeFiles/22s_final_proj.dir/searchQuery.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/22s_final_proj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
